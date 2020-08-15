bfdf06a99d186a2a49d3a56c1c9f253fd6834938
taufiqx404 <59577411+taufiqx404@users.noreply.github.com>
2020-08-15 20:49:56 +0800

    Update and rename README.md to hacker-lover.py

---
+++ hacker-lover.py
@@ -0,0 +1,78 @@
+mport os
+import sys
+import time
+
+os.system ( 'clear' )
+time.sleep(1)
+os.system ( 'figlet TERMUX' )
+print "================================="
+time.sleep(1)
+print "           TAUFIQ X              "
+time.sleep(1)
+print "              x                  "
+time.sleep(1)
+print "        PANGLIMA CYBER           "
+time.sleep(1)
+print "================================="
+print " AUTHOR : TAUFIQ X               "
+time.sleep(1)
+print " TEAM   : RED DEVIL OFF          "
+time.sleep(1)
+print "        RED DEVIL OFF            "
+print "================================="
+print
+time.sleep(1)
+print "[+] PILIH TOOLS NYA TOL"
+time.sleep(1)
+print "[1] BENGKEL FB"
+time.sleep(1)
+print "[2] SCAN IP"
+time.sleep(1)
+print "[3] DDOS"
+time.sleep(1)
+print "[4] TROJAN"
+time.sleep(1)
+print "[5] INSTALL BAHAN DULU BIAR WORK TOL"
+time.sleep(2)
+pilih = raw_input('[?] pilih ae gan :')
+if pilih =="1":
+       os.system( 'git clone https://github.com/taufiqx404/red.py' )
+       time.sleep(1)
+       print "[+] Selesai menginstall"
+       time.sleep(1)
+       print "[+] ketik cd red.py"
+       time.sleep(1)
+       print "[+] ketik python2 red.py"
+       time.sleep(5)
+elif pilih =="2":
+       os.system( 'git clone https://github.com/Bl4ckDr460n/ScanIp' )
+       time.sleep(1)
+       print "[+] selesai menginstall"
+       time.sleep
+       print "[+] ketik cd scanip"
+       time.sleep(1)
+       print "[+] ketik python2 scanip.py"
+elif pilih =="3":
+       os.system( 'git clone https://github.com/cyweb/hammer' )
+       time.sleep(1)
+       print "[+] selesai menginstall"
+       time.sleep(1)
+       print "[+] ketik cd hammer"
+       time.sleep(1)
+       print "[+] ketik python2 hammer.py"
+elif pilih =="4":
+       os.system( 'git clone https://github.com/justahackers/perusak' )
+       time.sleep(1)
+       print "[+] selesai menginstall"
+       time.sleep(1)
+       print "[+] ketik cd perusak"
+       time.sleep(1)
+       print "[8] ketik  python2 perusak.py"
+elif pilih =="5":
+       os.system('apt update && apt upgrade')
+       os.system('pkg install python2')
+       os.system('pkg install nano')
+       os.system('pkg install git')
+       os.system('pip2 install requests mechanize')
+       time.sleep(1)
+       print "[+]  SELESAI MENGINSTALL "
